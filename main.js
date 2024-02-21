9d0ffaf7bf93547dc18ee8d86f4bfe8ea545bafb
unknown <tmcfall.prsvr@gmail.com>
2024-02-21 12:20:14 -0500

    initial

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
 const URL = 'https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty'
 
 
-fetch
+function fetchData1(response){
+    let api1 = `https://hacker-news.firebaseio.com/v0/item/${response}.json?print=pretty`
+    return fetch(api1)
+}
+
+
+fetch(URL)
+.then((rawResponse)=>{
+    return rawResponse.json()
+})
